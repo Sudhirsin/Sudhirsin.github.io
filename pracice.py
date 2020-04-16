f3f5171f36d051db1be277d0850f3fe62f0cb4a5
Sudhirsin <lion.sud.k@gmail.com>
2020-04-16 20:13:26 +0530

    Circle updated

---
+++ pracice.py
@@ -0,0 +1,20 @@
+# l1 = [1, 2, 3, 4]
+# l2 = l1
+# l2[0] = 5
+# # l1 = [1, 2, 3, 4]
+# # l2 = [5, 2, 3, 4]
+# print(l1, l2)
+
+# arr = [2,1,4,5,9,8,6,10,10]
+
+# first = 0
+# second = 0
+# first = arr[0]
+# for i in range(1, len(arr)):
+#     if arr[i] > first:
+#         second = first
+#         first = arr[i]
+#     elif arr[i] > second:
+#         second = arr[i]
+# print(first, second)
+
